e5c766486faa3628dcfb2ff6f67fbdc880925451
Henok-Haile <henokhaile53@gmail.com>
2024-04-18 05:02:21 -0700

    Task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# A bash script that sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "Cohort 19 ALX School student" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current;}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
