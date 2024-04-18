c5a7bfb0ffe415a2d9c8428fae343f159f24a8c9
Henok-Haile <henokhaile53@gmail.com>
2024-04-18 06:07:34 -0700

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,12 +7,12 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-echo "Cohort 19 ALX School student" > /data/web_static/releases/test/index.html
+sudo echo "Cohort 19 ALX School student" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
