a47f16c2824123263885c37c641beeb28261d0c0
Henok-Haile <henokhaile53@gmail.com>
2024-04-18 04:02:29 -0700

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,14 +4,17 @@
 
 from fabric.api import local
 import time
+import os
 
 
 def do_pack():
     """Creates a tar gziiped archive of the folder of the web_static."""
     try:
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
+        now = time.strftime("%Y%m%d%H%M%S")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(now))
+        file_size = os.path.getsize("versions/web_static_{}.tgz".format(now))
+        print(f"web_static packed: versions/web_static_{now}.tgz -> {file_size}Bytes")
         return ("versions/web_static_{}.tgz".format(time.
                 strftime("%Y%m%d%H%M%S")))
     except:
