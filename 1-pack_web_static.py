7d8cc404a180cce6d9fccaa7ff75f04474d49e2e
Henok-Haile <henokhaile53@gmail.com>
2024-04-18 07:58:20 -0700

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,19 +3,27 @@
 # the web_static folder
 
 from fabric.api import local
-import time
+from datetime import datetime
 import os
-
 
 def do_pack():
     """Creates a tar gziiped archive of the folder of the web_static."""
     try:
-        local("mkdir -p versions")
-        now = time.strftime("%Y%m%d%H%M%S")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(now))
-        file_size = os.path.getsize("versions/web_static_{}.tgz".format(now))
-        print(f"web_static packed: versions/web_static_{now}.tgz -> {file_size}Bytes")
-        return ("versions/web_static_{}.tgz".format(time.
-                strftime("%Y%m%d%H%M%S")))
+        if not os.path.exists("web_static"):
+            print("Error: 'web_static' folder does not exist.")
+            return None
+
+        # Create the 'versions' directory if it doesn't exist
+        local("sudo mkdir -p versions")
+
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+
+        # Get the file size
+        file_size = os.path.getsize(file_name)
+
+        print(f"web_static packed: {file_name} -> {file_size}Bytes")
+        return file_name
     except:
         return None
